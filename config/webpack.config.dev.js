68ae95d1b9b6e50fa1e90fa0cb9452bc1e0676f3
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-14 16:15:09 +0100

    Set up file architecture for netlify deploy

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const config = require("./webpack.config.js");
+
+config.devServer = {
+  historyApiFallback: true,
+  port: 8080,
+  open: true,
+  liveReload: true,
+};
+
+config.devtool = "inline-source-map";
+
+module.exports = config;
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div id="app">
+      <main>
+        <h1>budFit</h1>
+      </main>
+    </div>
+  );
+};
