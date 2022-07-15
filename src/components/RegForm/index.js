905a415f1f52bd3d50cf44c726085bf4a081bb31
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-15 12:03:41 +0100

    start styling form and adding controlled form logic

--- src/components/RegForm/index.js
+++ src/components/RegForm/index.js
@@ -1,21 +1,61 @@
 import React, { useState } from "react";
+import { TextField } from "@mui/material";
 function RegForm() {
-  const [formdata, setFormData] = useState({
+  const [formData, setFormData] = useState({
     name: "",
     username: "",
     email: "",
     password: "",
+    passwordconfirmation: "",
   });
+
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   return (
     <div>
-      <form>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
+      <form noValidate autoComplete="off">
+        <TextField
+          id="name"
+          label="Name"
+          variant="filled"
+          value={formData.name}
+          onChange={onInputChange}
+          required
+        />
+        <TextField
           id="username"
-          name="username"
-          placeholder="That's not my name!"
+          label="Username"
+          variant="filled"
+          value={formData.username}
+          onChange={onInputChange}
+          required
+        />
+        <TextField
+          id="email"
+          label="Email"
+          variant="filled"
+          value={formData.email}
+          onChange={onInputChange}
+          required
+        />
+        <TextField
+          id="password"
+          label="Password"
+          variant="filled"
+          value={formData.password}
+          onChange={onInputChange}
+          required
+        />
+        <TextField
+          id="passwordconfirmation"
+          label="Confirm Your Password"
+          variant="filled"
+          value={formData.passwordconfirmation}
+          onChange={onInputChange}
+          required
         />
         <input type="submit" value="Update!" />
       </form>
