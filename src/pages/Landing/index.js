060df59af63458874d6847ad83b602670dee98f2
GMillerMc <gmiller04@yahoo.com>
2022-07-15 12:10:34 +0100

    add videos to assets

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import './index.css'
+import bgVideo1 from '../../assets/bgvideos/video.mp4'
 
 export const Landing = () => {
   return (
     <>
+      <div className="bg-container">
+        <video loop autoPlay>
+          <source 
+          src={bgVideo1} 
+          type="video/mp4"
+          />
+        </video>
+
+  
       <h1>budFit</h1>
+      </div>
     </>
   );
 };
