89bc09ac3ca847cb1f3b8a9bd71f31824e81b737
GMillerMc <gmiller04@yahoo.com>
2022-07-15 18:05:05 +0100

    make buttons responsive to changes

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,19 +1,12 @@
 import React from "react";
 import './index.css';
 import { HomeBtns } from '../../components'
-// import bgVideo1 from '../../assets/bgvideos/video.mp4'
+
 
 export const Landing = () => {
   return (
     <>
       <div className="bg-container">
-        {/* <video loop autoPlay>
-          <source 
-          src={bgVideo1} 
-          type="video/mp4"
-          />
-        </video> */}
-        
         
         <div className="btn-container">
           <HomeBtns />
