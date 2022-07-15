65d7f40556b0b8e0bec18d94deb0a0a4b93cbf32
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-15 13:30:03 +0100

    add stylesheet for register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { RegForm } from "../../components";
 import { SearchBtns } from "../../components";
 import { BackButton } from "../../components";
+import "./index.css";
 export const Register = () => {
   return (
-    <>
+    <div className="form-container">
       <h1>Register</h1>
       <RegForm />
-    </>
+    </div>
   );
 };
