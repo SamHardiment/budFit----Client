6b9291e853bc9fb11f6f247a54c0ec8b55e457fa
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-15 12:43:25 +0100

    add new check for password

--- src/components/RegForm/index.js
+++ src/components/RegForm/index.js
@@ -1,23 +1,47 @@
 import React, { useState } from "react";
-import { TextField } from "@mui/material";
+import axios from "axios";
+import { TextField, Button } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import AddIcon from "@mui/icons-material/Add";
 function RegForm() {
   const [formData, setFormData] = useState({
     name: "",
     username: "",
     email: "",
     password: "",
-    passwordconfirmation: "",
   });
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
+  // Form data input change
   const onInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
+  // To not include passconfirmation in formdata
+  const onPassConfirmationChange = (e) =>
+    setPasswordConfirmation(e.target.value);
+
+  // Send form submition to database
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.password == passwordConfirmation) {
+      if (
+        formData.name &&
+        formData.username &&
+        formData.email &&
+        formData.password
+      ) {
+        console.log(formData);
+      }
+    }
+    console.log("end of submit");
+  };
 
   return (
     <div>
-      <form noValidate autoComplete="off">
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
         <TextField
+          name="name"
           id="name"
           label="Name"
           variant="filled"
@@ -26,6 +50,7 @@ function RegForm() {
           required
         />
         <TextField
+          name="username"
           id="username"
           label="Username"
           variant="filled"
@@ -34,6 +59,7 @@ function RegForm() {
           required
         />
         <TextField
+          name="email"
           id="email"
           label="Email"
           variant="filled"
@@ -42,6 +68,7 @@ function RegForm() {
           required
         />
         <TextField
+          name="password"
           id="password"
           label="Password"
           variant="filled"
@@ -50,14 +77,18 @@ function RegForm() {
           required
         />
         <TextField
+          name="passwordconfirmation"
           id="passwordconfirmation"
           label="Confirm Your Password"
           variant="filled"
           value={formData.passwordconfirmation}
-          onChange={onInputChange}
+          onChange={onPassConfirmationChange}
           required
         />
-        <input type="submit" value="Update!" />
+        <Button variant="contained" type="submit">
+          <FontAwesomeIcon icon="fa-solid fa-plus" />
+          Create
+        </Button>
       </form>
     </div>
   );
