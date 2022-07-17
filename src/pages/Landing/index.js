00cd8990961c6413ecd0def0d34d7eafb816be12
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-17 15:06:48 +0100

    add new background pic

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,17 +1,22 @@
 import React from "react";
-import './index.css';
-import { HomeBtns } from '../../components'
-
+import "./index.css";
+import { HomeBtns } from "../../components";
+import image from "../../assets/images/bgbball.jpg";
 
 export const Landing = () => {
   return (
     <>
-      <div className="bg-container">
-        
+      <div
+        className="bg-container"
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
         <div className="btn-container">
           <HomeBtns />
         </div>
-      
       </div>
     </>
   );
