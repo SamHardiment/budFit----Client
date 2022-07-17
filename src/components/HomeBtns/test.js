63ef6b22590e305eef41d88c18969cbec4fa8f4e
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-17 12:01:50 +0100

    add mock function to test navigation to login and reg pages

--- src/components/HomeBtns/test.js
+++ src/components/HomeBtns/test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import { screen } from "@testing-library/react";
-import { HomeBtns } from ".";
+import HomeBtns from ".";
 
 const mockedUsedNavigate = jest.fn();
 jest.mock("react-router-dom", () => ({
@@ -16,9 +16,18 @@ describe("HomeBtns", () => {
   });
 
   it("User navigates to register page when register button is clicked", async () => {
-    const regbtn = screen.getByTestId("signInBtn");
-    await userEvent.click(button);
+    const regbtn = screen.getByRole("button", { name: "Register" });
+    await userEvent.click(regbtn);
     expect(regbtn).toBeInTheDocument();
-    expect(mockedUsedNavigate).toHaveBeenCalledWith("/Register");
+    expect(mockedUsedNavigate).toHaveBeenCalledTimes(1);
+    expect(mockedUsedNavigate).toHaveBeenCalledWith("/register");
+  });
+
+  it("User navigates to register page when register button is clicked", async () => {
+    const logbtn = screen.getByRole("button", { name: "Login" });
+    await userEvent.click(logbtn);
+    expect(logbtn).toBeInTheDocument();
+    expect(mockedUsedNavigate).toHaveBeenCalledTimes(1);
+    expect(mockedUsedNavigate).toHaveBeenCalledWith("/login");
   });
 });
