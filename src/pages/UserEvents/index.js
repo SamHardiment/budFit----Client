94442038f172cd3040541fb2186c2a62bac7cc31
Tom Millington <thomas.millington@mail.com>
2022-07-18 15:23:19 +0100

    add events page and update navbar

--- src/pages/UserEvents/index.js
+++ src/pages/UserEvents/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Fab } from '@mui/material';
+import { Add } from '@mui/icons-material';
 
 import { TopBar, EventPreview } from "../../components";
 import './index.css';
 
 export const UserEvents = () => {
+  const navigate = useNavigate();
+
   const [totalEvents, setTotalEvents] = useState(0);
-  const [events, setEvents] = useState([{activity:"Football", descr:"Some football"}, {activity:"Golf", descr:"Some golf"}]);
+  const [events, setEvents] = useState([{ activity: "Football", time: "1997-07-16T19:20:15", lastMessage: "This is the football message" }, { activity: "Golf", time: "1997-07-16T19:20:15", lastMessage: "This is the golf message" }]);
+
+  const handleClick = () =>{
+    navigate('/create')
+  }
 
   return (
     <>
@@ -15,10 +24,13 @@ export const UserEvents = () => {
         Joined Events: {totalEvents}
 
         <div id="chatsContainer">
-        {events.map((event) => (
-          <EventPreview key={Math.random()} activity={event.activity} descr={event.descr}/>
-        ))}
+          {events.map((event) => (
+            <EventPreview key={Math.random()} activity={event.activity} dateTime={event.time} lastMessage={event.lastMessage} />
+          ))}
         </div>
+        <Fab aria-label="newEvent" onClick={handleClick}>
+          <Add />
+        </Fab>
       </div>
     </>
   );
