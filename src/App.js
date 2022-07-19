facaa0acdffe2277e37387007f01eabe5c21de66
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-19 08:39:19 +0100

    install react-device-preview, add device emulator and find media problems

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
 import React from "react";
 import * as pages from "./pages";
 import { Routes, Route } from "react-router-dom";
-
-import { useAuthContext } from "./auth/index.js"
-
-
-
+import ReactDevicePreview from "react-device-preview";
+import { useAuthContext } from "./auth/index.js";
 
 import { NavBar } from "./components";
 
-import './App.css'
+import "./App.css";
 
 export const App = () => {
   const { user } = useAuthContext();
 
   return (
-    <div id="app">
-      <Routes>
-        <Route path="/" element={<pages.Landing />} />
-{/* 
+    <ReactDevicePreview device="iphonex" scale="1">
+      <div id="app">
+        <Routes>
+          <Route path="/" element={<pages.Landing />} />
+          {/* 
         {!user ? (
           <> */}
-        <Route path="/Login" element={<pages.Login />} />
-        <Route path="/Register" element={<pages.Register />} />
-        {/* </>
+          <Route path="/Login" element={<pages.Login />} />
+          <Route path="/Register" element={<pages.Register />} />
+          {/* </>
         ) : (
           <> */}
-        <Route path="/Searching" element={<pages.Searching />} />
-        <Route path="/Search" element={<pages.Search />} />
+          <Route path="/Searching" element={<pages.Searching />} />
+          <Route path="/Search" element={<pages.Search />} />
 
-        {/* </>
+          {/* </>
         )} */}
 
+          <Route path="/Account" element={<pages.Account />} />
+          <Route path="/Chat" element={<pages.ChatRoom />} />
+          <Route path="/Events" element={<pages.UserEvents />} />
+          <Route path="/Create" element={<pages.CreateEvent />} />
+          <Route path="/Safety" element={<pages.UserSafety />} />
+          <Route path="/Success" element={<pages.CreateEventSuccess />} />
 
-        <Route path="/Account" element={<pages.Account />} />
-        <Route path="/Chat" element={<pages.ChatRoom />} />
-        <Route path="/Events" element={<pages.UserEvents />} />
-        <Route path="/Create" element={<pages.CreateEvent />} />
-        <Route path="/Safety" element={<pages.UserSafety />} />
-        <Route path="/Success" element={<pages.CreateEventSuccess />} />
-        
-        <Route path="*" element={<pages.Error404 />} />
-
-
-      </Routes>
-      <NavBar />
-    </div>
+          <Route path="*" element={<pages.Error404 />} />
+        </Routes>
+        <NavBar />
+      </div>
+    </ReactDevicePreview>
   );
 };
