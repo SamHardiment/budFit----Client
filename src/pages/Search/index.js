fca6c558d186e5e424954cc4ef5a5f993bdf0f4e
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-19 12:53:39 +0100

    change preference to location, update icon

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect, useMemo, useRef } from "react";
 import TinderCard from "react-tinder-card";
 import { useSelector } from "react-redux";
-// import basketball from "./EventImages/basketball.png";
-// import basketball from './EventImages/basketball.png'
-// import cricket from './EventImages/cricket.png'
-// import cycling from './EventImages/cycling.png'
-// import football from './EventImages/football.png'
-// import golf from './EventImages/golf.png'
-// import hiking from './EventImages/hiking.png'
-// import running from './EventImages/running.png'
+
+// import basketball from './EventImages/basketball.jpg'
+// import cricket from './EventImages/cricket.jpg'
+// import cycling from './EventImages/cycling.jpg'
+// import football from './EventImages/football.jpg'
+// import golf from './EventImages/golf.jpg'
+// // import gym from './EventImages/gym.jpg'
+// import hiking from './EventImages/hiking.jpg'
+// import running from './EventImages/running.jpg'
 
 import { styled } from "@mui/material/styles";
 import { IconButton } from "@mui/material";
@@ -44,16 +45,15 @@ const UndoButton = styled(IconButton)({
   height: "100%",
 });
 
-// const images = [
-//   { name: "Running", img: running },
-//   { name: "Cycling", img: cycling },
-//   { name: "Football", img: football },
-//   { name: "Cricket", img: cricket },
-//   { name: "Gym", img: gym },
-//   { name: "Golf", img: golf },
-//   { name: "Hiking", img: hiking },
-//   { name: "Basketball", img: basketball },
-// ];
+// const images = [{ name: "Running", img: running },
+// { name: "Cycling", img: cycling },
+// { name: "Football", img: football },
+// { name: "Cricket", img: cricket },
+// // { name: "Gym", img: gym },
+// { name: "Golf", img: golf },
+// { name: "Hiking", img: hiking },
+// { name: "Basketball", img: basketball }
+// ]
 
 function Search() {
   const users = useSelector((state) => state.searchResults);
