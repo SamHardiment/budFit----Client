eadca1ee0961fc4c679267c787e86d557892a129
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-20 12:39:35 +0100

    remove headers from webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -5,12 +5,6 @@ config.devServer = {
   port: 8080,
   open: true,
   liveReload: true,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    "Access-Control-Allow-Headers":
-      "X-Requested-With, content-type, Authorization",
-  },
 };
 
 config.devtool = "inline-source-map";
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.css";
 import { HomeBtns, TopBar } from "../../components";
 import image from "../../assets/images/bgbball.jpg";
+import axios from "axios";
 
 export const Landing = () => {
+  // const updateObj = {
+  //   name: "works",
+  //   username: "maybe",
+  //   email: "sam@sam.com",
+  //   dob: "string",
+  //   preferences: "london",
+  //   picture: "test",
+  // };
+
+  // useEffect(() => {
+  //   console.log("useeEffect");
+
+  //   async function newEvent() {
+  //     console.log("before axios");
+
+  //     let resp = await axios.post("https://budfit.herokuapp.com/events", {
+  //       title: "e.target[0].value",
+  //       descr: "e.target[2].value",
+  //       date: "e.target[5].value",
+  //       activity: "e.target[7].value",
+  //       location: "e.target[11].value",
+  //       spaces: "e.target[13].value",
+  //     });
+  //   }
+  //   newEvent();
+  // }, []);
+
   return (
     <>
       <div
         className="bg-container"
         style={{
-          // backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8, url(${image}))`,
           backgroundImage: `url(${image})`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
