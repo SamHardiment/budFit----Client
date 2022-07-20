920722a0c52c367e4319b43ee53b4ab764747cde
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-20 13:12:08 +0100

    remove navbar from main

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import * as pages from "./pages";
 import { Routes, Route } from "react-router-dom";
 
-import { useAuthContext } from "./auth/index.js"
-
-
-
+import { useAuthContext } from "./auth/index.js";
 
 import { NavBar } from "./components";
 
-import './App.css'
+import "./App.css";
 
 export const App = () => {
   const { user } = useAuthContext();
@@ -21,29 +18,25 @@ export const App = () => {
 
         {!user ? (
           <>
-        <Route path="/Login" element={<pages.Login />} />
-        <Route path="/Register" element={<pages.Register />} />
-        </>
+            <Route path="/Login" element={<pages.Login />} />
+            <Route path="/Register" element={<pages.Register />} />
+          </>
         ) : (
           <>
-        <Route path="/Searching" element={<pages.Searching />} />
-        <Route path="/Search" element={<pages.Search />} />
-        <Route path="/Account" element={<pages.Account />} />
-        <Route path="/Chat" element={<pages.ChatRoom />} />
-        <Route path="/Events" element={<pages.UserEvents />} />
-        <Route path="/Create" element={<pages.CreateEvent />} />
-        <Route path="/Safety" element={<pages.UserSafety />} />
-        <Route path="/Success" element={<pages.CreateEventSuccess />} />
-        <Route path="/u/:id" element={<pages.UserDetails />} />
+            <Route path="/Searching" element={<pages.Searching />} />
+            <Route path="/Search" element={<pages.Search />} />
+            <Route path="/Account" element={<pages.Account />} />
+            <Route path="/Chat" element={<pages.ChatRoom />} />
+            <Route path="/Events" element={<pages.UserEvents />} />
+            <Route path="/Create" element={<pages.CreateEvent />} />
+            <Route path="/Safety" element={<pages.UserSafety />} />
+            <Route path="/Success" element={<pages.CreateEventSuccess />} />
+            <Route path="/u/:id" element={<pages.UserDetails />} />
 
-        <Route path="*" element={<pages.Error404 />} />
-
-
-        </>
+            <Route path="*" element={<pages.Error404 />} />
+          </>
         )}
-
       </Routes>
-      <NavBar />
     </div>
   );
 };
