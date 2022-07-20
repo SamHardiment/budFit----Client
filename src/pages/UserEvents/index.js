25f6a8038c14bcc6aed6efab257618818d69ff1b
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-20 14:46:15 +0100

    add test gget request in userEvents

--- src/pages/UserEvents/index.js
+++ src/pages/UserEvents/index.js
@@ -1,42 +1,78 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Fab } from '@mui/material';
-import { Add, Warning } from '@mui/icons-material';
+import axios from "axios";
+import { Fab } from "@mui/material";
+import { Add, Warning } from "@mui/icons-material";
 
 import { TopBar, EventPreview } from "../../components";
-import './index.css';
+import "./index.css";
 
 export const UserEvents = () => {
   const navigate = useNavigate();
 
   const [totalEvents, setTotalEvents] = useState(0);
-  const [events, setEvents] = useState([{ activity: "Football", time: "1997-07-16T19:20:15", lastMessage: "This is the football message" }, { activity: "Golf", time: "1997-07-16T19:20:15", lastMessage: "This is the golf message" }]);
+  const [events, setEvents] = useState([
+    {
+      activity: "Football",
+      time: "1997-07-16T19:20:15",
+      lastMessage: "This is the football message",
+    },
+    {
+      activity: "Golf",
+      time: "1997-07-16T19:20:15",
+      lastMessage: "This is the golf message",
+    },
+  ]);
 
-  const handleCreateClick = () =>{
-    navigate('/create')
-  }
+  // Get user events
+  useEffect(() => {
+    getUserEvents("2");
+  }, []);
 
-  const handleSafetyClick = () =>{
-    navigate('/safety')
+  async function getUserEvents(id) {
+    const data = axios.get(`https://budfit.herokuapp.com/events/${id}/`);
+    console.log(data);
   }
+  // Handle naviagtion
+
+  const handleCreateClick = () => {
+    navigate("/create");
+  };
+
+  const handleSafetyClick = () => {
+    navigate("/safety");
+  };
 
   return (
     <>
       <TopBar />
-      <div id="chatsPageContainer" >
+      <div id="chatsPageContainer">
         <h4>Joined Events: {totalEvents}</h4>
 
         <div id="chatsContainer">
           {events.map((event) => (
-            <EventPreview key={Math.random()} activity={event.activity} dateTime={event.time} lastMessage={event.lastMessage} />
+            <EventPreview
+              key={Math.random()}
+              activity={event.activity}
+              dateTime={event.time}
+              lastMessage={event.lastMessage}
+            />
           ))}
         </div>
-        <Fab color="secondary" aria-label="newEvent" onClick={handleCreateClick}>
-          <Add data-testid="newBtn"/>
+        <Fab
+          color="secondary"
+          aria-label="newEvent"
+          onClick={handleCreateClick}
+        >
+          <Add data-testid="newBtn" />
         </Fab>
-        <Fab color="warning" aria-label="safetyAdvice" onClick={handleSafetyClick}>
-          <Warning data-testid="warnBtn"/>
+        <Fab
+          color="warning"
+          aria-label="safetyAdvice"
+          onClick={handleSafetyClick}
+        >
+          <Warning data-testid="warnBtn" />
         </Fab>
       </div>
     </>
