7aa32eea59293507b6c815c849e2d37cb627d503
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-21 21:46:33 +0100

    adds stytling to event description2

--- src/components/EventView/index.js
+++ src/components/EventView/index.js
@@ -2,57 +2,59 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Container, Typography } from "@mui/material";
 
-
 function EventView(props) {
   const navigate = useNavigate();
-  const { activity, dateTime, title, attendees, location, description, spaces } = props;
+  const {
+    activity,
+    dateTime,
+    title,
+    attendees,
+    location,
+    description,
+    spaces,
+  } = props;
 
   let x = Date.parse(`${dateTime}`).toString().slice(0, 15);
-  
+
   const handleUsernameClickInEventView = (id) => {
-    navigate(`/u/${id}`)
-  }
+    navigate(`/u/${id}`);
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 4,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h5">{title}</Typography>
-        <Typography variant="p" color="textSecondary">
-          {activity}
+    <div className="inv-event-detail">
+      <Typography variant="h5">{title}</Typography>
+      <Typography variant="h6" color="textSecondary">
+        {activity}
+      </Typography>
+      <Typography variant="body1" color="textSecondary">
+        {x}
+      </Typography>
+      <div className="sub-event-details">
+        <div className="sub-event-desc">
+          <Typography variant="body1">{description}</Typography>
+        </div>
+        <Typography variant="body1">
+          This event is taking place in: {location}
         </Typography>
-        <Typography variant="p" color="textSecondary">
-          {x}
-        </Typography>
-        <Typography variant="p" color="textSecondary">
-          {location}
-        </Typography>
-        <Typography variant="p" color="textSecondary">
-          {description}
-        </Typography>
-        <Typography variant="p" color="textSecondary">
+      </div>
+
+      <div className="sub-event-spaces">
+        <Typography variant="body1">
           Spaces filled: {attendees.length}/{spaces}
         </Typography>
-        <Typography variant="p" color="textSecondary">
-          Users attending:
-        </Typography>
+        <Typography variant="body1">Users attending:</Typography>
         <ul>
-        {attendees.map((user) => (
-          <li key={user.username} onClick={()=>handleUsernameClickInEventView(user.user_id)}>
-            <Typography variant="p" color="textSecondary">
-              {user.username}
-            </Typography>
-          </li>
-        ))}
+          {attendees.map((user) => (
+            <li
+              key={user.username}
+              onClick={() => handleUsernameClickInEventView(user.user_id)}
+            >
+              <Typography variant="body1">{user.username}</Typography>
+            </li>
+          ))}
         </ul>
-      </Box>
-    </Container>
+      </div>
+    </div>
   );
 }
 
